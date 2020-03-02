6ce378ac719dbf84a28a0d028b82b4b7953f90c3
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2020-03-02 20:54:53 +0800

    ex3: cost function

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -5,13 +5,6 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
@@ -36,17 +29,28 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+    % Initialize some useful values
+    m = length(y); % number of training examples
+
+    % You need to return the following variables correctly 
+    J = 0;
+    grad = zeros(size(theta));
+    
+    matrix_thetaX = X * theta;
+    
+    prediction = sigmoid(matrix_thetaX);                % hypothesis function, prediction, h_theta(x)
+    
+    grad = 1/m * (X' * (prediction - y));        % unregularized gradient for logistic regression
+    temp = theta; 
+    temp(1) = 0;                                 % because we don't add anything for j = 0
+     
+    J = ((-y' * log(prediction) - (1 - y)' * log(1 - prediction)) / m) + (lambda/ (2*m) * temp'* temp);
+     
+    grad = grad + lambda * temp/m; 
 
 
+  % =============================================================
 
-
-
-
-
-
-
-% =============================================================
-
-grad = grad(:);
+  grad = grad(:);
 
 end
